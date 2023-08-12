ea19d67aec1c03ee1ddafc6f9883724a21947c44
Takayoshi Watanabe <w1543212@gmail.com>
2023-08-12 17:19:21 +0900

    update: 文言

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <PageLayoutSkill title="職務経歴書" date="2023年3月29日" name="渡辺貴義">
+    <PageLayoutSkill title="職務経歴書" date="2023年8月11日" name="渡辺貴義">
       <template #col-main>
         <SkillSheet/>
       </template>
